d334091c43f4cc87b129c1296cc615d04c83989b
Kajetan <kajetan.loska@op.pl>
2022-06-09 15:02:29 +0200

    .

--- src/gameboard.js
+++ src/gameboard.js
@@ -10,5 +10,42 @@ class Gameboard {
       }
     }
   }
+  placeShip(ship, coords, orientation) {
+    if (this.#isPlaceAvailable(ship.length, coords, orientation) === false) return;
+    this.#createShip(ship.length, coords, orientation);
+  }
+  #createShip(length, coords, orientation) {
+    if (orientation === 'vertical') {
+      for (let i = 0; i < length; i++) {
+        this.board[i + coords[0]][coords[1]] = 'S';
+      }
+    } else if (orientation === 'horizontal') {
+      for (let i = 0; i < length; i++) {
+        this.board[coords[0]][i + coords[1]] = 'S';
+      }
+    }
+  }
+  #isPlaceAvailable(length, coords, orientation) {
+    // if (orientation !== 'vertical' || orientation !== 'horizontal') return false;
+    if (orientation === 'vertical') {
+      return this.board.every(e => {
+        console.log(e);
+        for (let i = 0; i < length; i++) {
+          e[i + coords[0]][coords[1]] === '';
+        }
+      });
+    } else {
+      return this.board.every(e => {
+        for (let i = 0; i < length; i++) {
+          e[coords[0]][i + coords[1]] === '';
+        }
+      });
+    }
+  }
 }
+// let newGameboard = new Gameboard();
+// newGameboard.init();
+// newGameboard.placeShip([0, 1]);
+// console.log(newGameboard.board);
+
 module.exports = Gameboard;
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,5 +1,5 @@
 let Gameboard = require('../gameboard');
-
+let Ship = require('../ship');
 describe('gameboard', () => {
   it('creates a gameboard', () => {
     let newGameboard = new Gameboard();
@@ -9,8 +9,25 @@ describe('gameboard', () => {
     expect(newGameboard.board[0][0]).toBeDefined();
     expect(newGameboard.board[10]).toBeUndefined();
   });
-  it('places a ship', () => {
+  it('places a 1 length ship', () => {
     let newGameboard = new Gameboard();
     newGameboard.init();
+    let newShip = { hits: 0, length: 1 };
+    newGameboard.placeShip(newShip, [0, 1], 'vertical');
+    newGameboard.placeShip(newShip, [2, 3], 'vertical');
+    expect(newGameboard.board[0][1]).toEqual('S');
+    expect(newGameboard.board[2][3]).toEqual('S');
+  });
+  it('places a 2 or more length ship', () => {
+    let newGameboard = new Gameboard();
+    newGameboard.init();
+    let newShip = { hits: 0, length: 2 };
+    newGameboard.placeShip(newShip, [0, 1], 'vertical');
+    newGameboard.placeShip(newShip, [5, 6], 'horizontal');
+    console.log(newGameboard.board);
+    expect(newGameboard.board[0][1]).toEqual('S');
+    expect(newGameboard.board[1][1]).toEqual('S');
+    expect(newGameboard.board[5][6]).toEqual('S');
+    expect(newGameboard.board[5][7]).toEqual('S');
   });
 });
