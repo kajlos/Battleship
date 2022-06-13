d2d944a0b04c49e3587c83be2d60777a40ef057c
Kajetan <kajetan.loska@op.pl>
2022-06-13 15:29:13 +0200

    added marking

--- src/gameboard.js
+++ src/gameboard.js
@@ -42,13 +42,123 @@ class Gameboard {
     }
     return false;
   }
-  #markSpotsAfertPlace(length, coords, orientation) {}
+  #markSpotsAfertPlace(length, coords, orientation) {
+    let row = parseFloat(coords[0]);
+    let column = parseFloat(coords[1]);
+    if (orientation === 'vertical') {
+      if (length === 1) {
+        this.#markSpot([row - 1, column], '#');
+        this.#markSpot([row - 1, column - 1], '#');
+        this.#markSpot([row - 1, column + 1], '#');
+        this.#markSpot([row, column - 1], '#');
+        this.#markSpot([row, column + 1], '#');
+        this.#markSpot([row + 1, column], '#');
+        this.#markSpot([row + 1, column + 1], '#');
+        this.#markSpot([row + 1, column - 1], '#');
+      } else if (length === 2) {
+        this.#markSpot([row - 1, column], '#');
+        this.#markSpot([row - 1, column - 1], '#');
+        this.#markSpot([row - 1, column + 1], '#');
+        this.#markSpot([row, column - 1], '#');
+        this.#markSpot([row, column + 1], '#');
+        this.#markSpot([row + 1, column + 1], '#');
+        this.#markSpot([row + 1, column - 1], '#');
+        this.#markSpot([row + 2, column + 1], '#');
+        this.#markSpot([row + 2, column - 1], '#');
+        this.#markSpot([row + 2, column], '#');
+      } else if (length === 3) {
+        this.#markSpot([row - 1, column], '#');
+        this.#markSpot([row - 1, column - 1], '#');
+        this.#markSpot([row - 1, column + 1], '#');
+        this.#markSpot([row, column - 1], '#');
+        this.#markSpot([row, column + 1], '#');
+        this.#markSpot([row + 1, column + 1], '#');
+        this.#markSpot([row + 1, column - 1], '#');
+        this.#markSpot([row + 2, column + 1], '#');
+        this.#markSpot([row + 2, column - 1], '#');
+        this.#markSpot([row + 3, column + 1], '#');
+        this.#markSpot([row + 3, column - 1], '#');
+        this.#markSpot([row + 3, column], '#');
+      } else if (length === 4) {
+        this.#markSpot([row - 1, column], '#');
+        this.#markSpot([row - 1, column - 1], '#');
+        this.#markSpot([row - 1, column + 1], '#');
+        this.#markSpot([row, column - 1], '#');
+        this.#markSpot([row, column + 1], '#');
+        this.#markSpot([row + 1, column + 1], '#');
+        this.#markSpot([row + 1, column - 1], '#');
+        this.#markSpot([row + 2, column + 1], '#');
+        this.#markSpot([row + 2, column - 1], '#');
+        this.#markSpot([row + 3, column + 1], '#');
+        this.#markSpot([row + 3, column - 1], '#');
+        this.#markSpot([row + 4, column + 1], '#');
+        this.#markSpot([row + 4, column - 1], '#');
+        this.#markSpot([row + 4, column], '#');
+      }
+    } else if (orientation === 'horizontal') {
+      if (length === 1) {
+        this.#markSpot([row - 1, column], '#');
+        this.#markSpot([row - 1, column - 1], '#');
+        this.#markSpot([row - 1, column + 1], '#');
+        this.#markSpot([row, column - 1], '#');
+        this.#markSpot([row, column + 1], '#');
+        this.#markSpot([row + 1, column], '#');
+        this.#markSpot([row + 1, column + 1], '#');
+        this.#markSpot([row + 1, column - 1], '#');
+      } else if (length === 2) {
+        this.#markSpot([row - 1, column - 1], '#');
+        this.#markSpot([row, column - 1], '#');
+        this.#markSpot([row + 1, column - 1], '#');
+        this.#markSpot([row + 1, column], '#');
+        this.#markSpot([row - 1, column], '#');
+        this.#markSpot([row + 1, column + 1], '#');
+        this.#markSpot([row - 1, column + 1], '#');
+        this.#markSpot([row + 1, column + 2], '#');
+        this.#markSpot([row, column + 2], '#');
+        this.#markSpot([row - 1, column + 2], '#');
+      } else if (length === 3) {
+        this.#markSpot([row - 1, column - 1], '#');
+        this.#markSpot([row, column - 1], '#');
+        this.#markSpot([row + 1, column - 1], '#');
+        this.#markSpot([row + 1, column], '#');
+        this.#markSpot([row - 1, column], '#');
+        this.#markSpot([row + 1, column + 1], '#');
+        this.#markSpot([row - 1, column + 1], '#');
+        this.#markSpot([row + 1, column + 2], '#');
+        this.#markSpot([row - 1, column + 2], '#');
+        this.#markSpot([row - 1, column + 3], '#');
+        this.#markSpot([row + 1, column + 3], '#');
+        this.#markSpot([row, column + 3], '#');
+      } else if (length === 4) {
+        this.#markSpot([row - 1, column - 1], '#');
+        this.#markSpot([row, column - 1], '#');
+        this.#markSpot([row + 1, column - 1], '#');
+        this.#markSpot([row + 1, column], '#');
+        this.#markSpot([row - 1, column], '#');
+        this.#markSpot([row + 1, column + 1], '#');
+        this.#markSpot([row - 1, column + 1], '#');
+        this.#markSpot([row + 1, column + 2], '#');
+        this.#markSpot([row - 1, column + 2], '#');
+        this.#markSpot([row - 1, column + 3], '#');
+        this.#markSpot([row + 1, column + 3], '#');
+        this.#markSpot([row - 1, column + 4], '#');
+        this.#markSpot([row + 1, column + 4], '#');
+        this.#markSpot([row, column + 4], '#');
+      }
+    }
+    return;
+  }
+  #markSpot(coords, mark) {
+    let row = parseFloat(coords[0]);
+    let column = parseFloat(coords[1]);
+    if (row < 0 || row > 9 || column < 0 || column > 9) return;
+    this.board[row][column] = mark;
+  }
 }
 
 // let newGameboard = new Gameboard();
 // newGameboard.init();
-// let newShip = { hits: 0, length: 2 };
-// newGameboard.placeShip(newShip, [5, 6], 'vertical');
+// let newShip = new Ship(3);
 // newGameboard.placeShip(newShip, [5, 6], 'vertical');
 // console.log(newGameboard.board);
 
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -12,7 +12,7 @@ describe('gameboard', () => {
   it('places a 1 length ship', () => {
     let newGameboard = new Gameboard();
     newGameboard.init();
-    let newShip = { hits: 0, length: 1 };
+    let newShip = new Ship(1);
     newGameboard.placeShip(newShip, [0, 1], 'vertical');
     newGameboard.placeShip(newShip, [2, 3], 'vertical');
     expect(newGameboard.board[0][1]).toEqual('S');
@@ -21,34 +21,30 @@ describe('gameboard', () => {
   it('places a 2 or more length ship', () => {
     let newGameboard = new Gameboard();
     newGameboard.init();
-    let newShip = { hits: 0, length: 2 };
-    let newShip2 = { hits: 0, length: 3 };
+    let newShip = new Ship(2);
+    let newShip2 = new Ship(3);
     newGameboard.placeShip(newShip, [0, 1], 'vertical');
     newGameboard.placeShip(newShip2, [5, 6], 'horizontal');
-    newGameboard.placeShip(newShip, [1, 8], 'horizontal');
     expect(newGameboard.board[0][1]).toEqual('S');
     expect(newGameboard.board[1][1]).toEqual('S');
     expect(newGameboard.board[5][6]).toEqual('S');
     expect(newGameboard.board[5][7]).toEqual('S');
     expect(newGameboard.board[5][8]).toEqual('S');
-    expect(newGameboard.board[1][8]).toEqual('S');
-    expect(newGameboard.board[1][9]).toEqual('S');
   });
   it("doesn't create ship if ship would be outside gameboard", () => {
     let newGameboard = new Gameboard();
     newGameboard.init();
-    let newShip = { hits: 0, length: 2 };
-    let newShip2 = { hits: 0, length: 2 };
+    let newShip = new Ship(2);
+    let newShip2 = new Ship(2);
     newGameboard.placeShip(newShip, [9, 1], 'vertical');
     newGameboard.placeShip(newShip2, [1, 9], 'horizontal');
     expect(newGameboard.board[9][1]).toEqual('');
     expect(newGameboard.board[1][9]).toEqual('');
   });
-  it('marks spots when placed ship', () => {
+  it('marks spots when placed 1 length ship', () => {
     let newGameboard = new Gameboard();
     newGameboard.init();
-    let newShip = { hits: 0, length: 1 };
-    let newShip2 = { hits: 0, length: 2 };
+    let newShip = new Ship(1);
     newGameboard.placeShip(newShip, [5, 5], 'vertical');
     expect(newGameboard.board[5][5]).toEqual('S');
     expect(newGameboard.board[4][5]).toEqual('#');
@@ -59,5 +55,69 @@ describe('gameboard', () => {
     expect(newGameboard.board[6][5]).toEqual('#');
     expect(newGameboard.board[6][6]).toEqual('#');
     expect(newGameboard.board[6][4]).toEqual('#');
+  });
+  it('marks spots vertically when placed 2 length ship', () => {
+    let newGameboard = new Gameboard();
+    newGameboard.init();
+    let newShip = new Ship(2);
+    newGameboard.placeShip(newShip, [5, 5], 'vertical');
+    expect(newGameboard.board[5][5]).toEqual('S');
+    expect(newGameboard.board[6][5]).toEqual('S');
+    expect(newGameboard.board[4][4]).toEqual('#');
+    expect(newGameboard.board[4][5]).toEqual('#');
+    expect(newGameboard.board[4][6]).toEqual('#');
+    expect(newGameboard.board[5][4]).toEqual('#');
+    expect(newGameboard.board[5][6]).toEqual('#');
+    expect(newGameboard.board[6][6]).toEqual('#');
+    expect(newGameboard.board[6][4]).toEqual('#');
+    expect(newGameboard.board[7][4]).toEqual('#');
+    expect(newGameboard.board[7][5]).toEqual('#');
+    expect(newGameboard.board[7][6]).toEqual('#');
+  });
+  it('marks spot horizontally when placed 2 length ship', () => {
+    let newGameboard = new Gameboard();
+    newGameboard.init();
+    let newShip = new Ship(2);
+    newGameboard.placeShip(newShip, [5, 5], 'horizontal');
+    expect(newGameboard.board[5][5]).toEqual('S');
+    expect(newGameboard.board[5][6]).toEqual('S');
+    expect(newGameboard.board[5][4]).toEqual('#');
+    expect(newGameboard.board[4][4]).toEqual('#');
+    expect(newGameboard.board[6][4]).toEqual('#');
+    expect(newGameboard.board[4][5]).toEqual('#');
+    expect(newGameboard.board[6][5]).toEqual('#');
+    expect(newGameboard.board[4][6]).toEqual('#');
+    expect(newGameboard.board[6][6]).toEqual('#');
+    expect(newGameboard.board[4][7]).toEqual('#');
+    expect(newGameboard.board[5][7]).toEqual('#');
+    expect(newGameboard.board[6][7]).toEqual('#');
+  });
+  it('marks spot vertically when placed 3 length ship ', () => {
+    let newGameboard = new Gameboard();
+    newGameboard.init();
+    let newShip = new Ship(3);
+    newGameboard.placeShip(newShip, [7, 0], 'vertical');
+    expect(newGameboard.board[7][0]).toEqual('S');
+    expect(newGameboard.board[8][0]).toEqual('S');
+    expect(newGameboard.board[9][0]).toEqual('S');
+    expect(newGameboard.board[6][0]).toEqual('#');
+    expect(newGameboard.board[6][1]).toEqual('#');
+    expect(newGameboard.board[7][1]).toEqual('#');
+    expect(newGameboard.board[8][1]).toEqual('#');
+    expect(newGameboard.board[9][1]).toEqual('#');
+  });
+  it('marks spot horizontally when placed 3 length ship', () => {
+    let newGameboard = new Gameboard();
+    newGameboard.init();
+    let newShip = new Ship(3);
+    newGameboard.placeShip(newShip, [9, 7], 'horizontal');
+    expect(newGameboard.board[9][7]).toEqual('S');
+    expect(newGameboard.board[9][8]).toEqual('S');
+    expect(newGameboard.board[9][9]).toEqual('S');
+    expect(newGameboard.board[8][6]).toEqual('#');
+    expect(newGameboard.board[9][6]).toEqual('#');
+    expect(newGameboard.board[8][7]).toEqual('#');
+    expect(newGameboard.board[8][8]).toEqual('#');
+    expect(newGameboard.board[8][9]).toEqual('#');
   });
 });
