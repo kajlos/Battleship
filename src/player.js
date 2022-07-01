4f86c0e88d2e60bd8f9a671b67ae8c9ce8841590
Kajetan <kajetan.loska@op.pl>
2022-07-01 15:01:29 +0200

    .

--- src/player.js
+++ src/player.js
@@ -7,6 +7,6 @@ class Player {
   attack(targetPlayer, coords) {
     targetPlayer.board.receiveAttack(coords);
   }
-  randomAttack() {}
+  randomAttack(targetPlayer, coords) {}
 }
 module.exports = Player;
